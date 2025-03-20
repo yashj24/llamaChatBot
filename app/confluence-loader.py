4efb9f7455a4689bc41442ed32bd4dc2f3da5933
unknown <yashhj45@gmail.com>
2025-03-20 23:42:26 +0530

    added pages title

--- app/confluence-loader.py
+++ app/confluence-loader.py
@@ -1,12 +1,24 @@
-
 from atlassian import Confluence
 from bs4 import BeautifulSoup
 
 confluence = Confluence(
-   url = "https://openbanking.atlassian.net/wiki/",
+   url = "https://uclalibrary.atlassian.net/wiki/",
    username = "",
    password = ""
 )
+def getAllSpaceKeys(confluence):
+    print("inside getAllSpaceKeys")
+    start = 0
+    limit = 100
+    spaceId = []
+    while True:
+        spacesBatch = confluence.get_all_spaces(start , limit)
+        for space in spacesBatch["results"]:
+            spaceId.append(space['key'])
+        start += limit
+        if len(spacesBatch["results"]) < limit:
+            break
+    return spaceId
 
 def getAllPagesOfSpace(confluence , spaceKey):
     start = 0
@@ -21,15 +33,17 @@ def getAllPagesOfSpace(confluence , spaceKey):
     return _all_pages
 
 
-allpages = getAllPagesOfSpace(confluence , "AD")
+allpages = getAllPagesOfSpace(confluence , "LITKB")
 print(len(allpages))
 file = open("spacedata.txt" , "a" , encoding = "utf-8")
 print("Writing space content in file")
 for page in allpages:
     page_content = confluence.get_page_by_id(page['id'],expand ="body.storage")
+    page_title = page_content["title"]
     soup = BeautifulSoup(page_content["body"]["storage"]["value"] , "html.parser")
-    file.write(page["id"])
-    file.write(soup.get_text())
+    file.write("Page id : "+ page["id"])
+    file.write("Page Title: " + page_title )
+    file.write("Page Content : " + soup.get_text())
     file.write("\n")
 
 file.close()
